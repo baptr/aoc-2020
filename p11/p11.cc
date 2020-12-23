477936e8ca5b2ccbc179580300b6959030534fd7
Bryce Petrini <baptr01@gmail.com>
2020-12-22 23:58:40 -0800

    P11 part 2 done

--- p11/p11.cc
+++ p11/p11.cc
@@ -58,6 +58,54 @@ floorMap proc(floorMap prev) {
   return out;
 }
 
+// Returns whether the first chair in the viz direction is occupied.
+bool occupiedViz(const floorMap& m, int x, int y, int xDir, int yDir) {
+  for (;;) {
+    x += xDir;
+    y += yDir;
+    if (y < 0 || y >= m.size()) break;
+    vector<space> row = m[y];
+    if (x < 0 || x >= row.size()) break;
+    space s = row[x];
+    if (s == kOccupied) return true;
+    if (s == kEmpty) return false;
+  }
+  return false; // all floor
+}
+
+floorMap proc2(const floorMap& prev) {
+  floorMap out;
+  for (int j = 0; j < prev.size(); ++j) {
+    vector<space> row = prev[j];
+    vector<space> outRow;
+    for (int i = 0; i < row.size(); ++i) {
+      space now = row[i];
+      if (now == kFloor) {
+        outRow.push_back(now);
+        continue;
+      }
+      int neighbors = 0;
+      for (int x = -1; x <= 1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+          if (x == 0 && y == 0) continue;
+          if (occupiedViz(prev, i, j, x, y)) {
+            neighbors++;
+            if (neighbors >= 5) break;
+          }
+        }
+      }
+      if (now == kEmpty && neighbors == 0) {
+        now = kOccupied;
+      } else if (now == kOccupied && neighbors >= 5) {
+        now = kEmpty;
+      }
+      outRow.push_back(now);
+    }
+    out.push_back(outRow);
+  }
+  return out;
+}
+
 void dump(floorMap floor) {
   for (auto row = floor.cbegin(); row != floor.cend(); ++row) {
     for (auto it = row->cbegin(); it != row->cend(); ++it) {
@@ -89,7 +137,7 @@ int main(int argc, char** argv) {
 
   for (unsigned int i = 1;; i++) {
     // dump(floor);
-    floorMap next = proc(floor);
+    floorMap next = proc2(floor);
     if (next == floor) {
       cout << "Match after " << i << " iterations\n";
       break;
