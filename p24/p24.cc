57a201cd6d7ff1e7d65d84f4c0813b5c83606337
Bryce Petrini <baptr01@gmail.com>
2020-12-24 00:38:57 -0800

    P24.2 done

--- p24/p24.cc
+++ p24/p24.cc
@@ -32,7 +32,30 @@ struct Pos {
     if (y == b.y) return x < b.x;
     return y < b.y;
   }
+
+  vector<Pos> neighbors() const {
+    return {
+      {x-1, y},
+        {x+1, y},
+        {x, y+1},
+        {x-1, y+1},
+        {x, y-1},
+        {x+1, y-1},
+    };
+  }
 };
+
+const set<Pos> checkList(const map<Pos, Tile>& floor) {
+  set<Pos> out;
+  for (auto &[p, t] : floor) {
+    if (t.black) {
+      out.emplace(p);
+      const auto n = p.neighbors();
+      out.insert(n.begin(), n.end());
+    }
+  }
+  return out;
+}
 
 int main(int argc, char** argv) {
   ifstream inFile;
@@ -74,8 +97,45 @@ int main(int argc, char** argv) {
   for (auto &[p, t] : floor) {
     if (t.black) numBlack++;
   }
-
   cout << "Black tiles: " << numBlack << "\n";
+
+  for (int day = 1; day <= 100; ++day) {
+    // since the floor structure is sparse and something like 40x40, it feels
+    // wrong to iterate over the whole thing.  ~ugly option: make a todo list
+    // of positions of every black tile and everything adjacent to them.
+    // then step through that list, checking rules.
+    const set<Pos>& check = checkList(floor);
+    map<Pos,Tile> next;
+    int blackTomorrow = 0;
+    for (const Pos& p : check) {
+      int count = 0;
+      if (floor[p].black) { // 0 or more than 2 -> flip to white
+        for (const Pos& n : p.neighbors()) {
+          if (floor[n].black) {
+            count++;
+            if (count > 2) break;
+          }
+        }
+        if (count == 0 || count > 2) {
+          // flip to white.. no need to mention it
+        } else { // stays black tomorrow
+          next[p] = Tile{true};
+          blackTomorrow++;
+        }
+      } else { // exactly 2 -> flip to black
+        int count = 0;
+        for (const Pos& n : p.neighbors()) {
+          if (floor[n].black) count++;
+        }
+        if (count == 2) {
+          next[p] = Tile{true};
+          blackTomorrow++;
+        }
+      }
+    }
+    cout << "Day[" << day << "] : checking " << check.size() << " tiles, " << blackTomorrow << " will be black\n";
+    floor = next;
+  }
 
   return 0;
 }
